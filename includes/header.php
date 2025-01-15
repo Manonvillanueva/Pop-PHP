25e0b7b5f59ca3d783bcff47593e3f5f2f06ec98
ManonVillanueva <g.manon14@gmail.com>
2025-01-15 16:31:10 +0100

    header

---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<div class="nav-container">
+    <!-- TITLE  -->
+    <h1 class="site-title">POP MANIA</h1>
+
+    <!-- SEARCH INPUT  -->
+    <form action="">
+        <input type="text" placeholder="Rechercher ..." aria-label="search bar">
+    </form>
+
+    <!-- LINK NAV -->
+    <nav>
+        <!-- Utilisation d'aria label lorsque tu veux fournir une description d'un élément qui n'a pas de texte visible comme un bouton ou une icône ... Alt s'utilise uniquement sur les img -->
+        <ul class="nav-link">
+            <!-- Home  -->
+            <li><a href="index.php" aria-label="home page">
+                    <i class="fa-solid fa-house"></i>
+                </a></li>
+            <!-- Profil  -->
+            <li><a href="../pages/connexion.php" aria-label="profil">
+                    <i class="fa-solid fa-user"></i>
+                </a></li>
+            <!-- Basket  -->
+            <li><a href="../pages/basket.php" aria-label="basket">
+                    <i class="fa-solid fa-basket-shopping"></i>
+                </a></li>
+        </ul>
+    </nav>
+</div>
+
+<style>
+    .nav-container {
+        display: flex;
+    }
+
+    .nav-link {
+        display: flex;
+        list-style: none;
+    }
+</style>
--- index.php
+++ index.php
@@ -2,12 +2,26 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
     <title>POP</title>
 </head>
 
 <body>
+    <header>
+        <?php include "./includes/header.php" ?>
+    </header>
 </body>
 
 </html>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+</style>
