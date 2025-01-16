926f0f46912a9803c8355806d59c8df947f34c8b
ManonVillanueva <g.manon14@gmail.com>
2025-01-16 13:44:38 +0100

    create bdd and add products

---
+++ sql/bdd.sql
@@ -0,0 +1,13 @@
+INSERT INTO articles (titre, prix, stock, description, image_url, univers, disponibilite, taille, number)
+VALUES
+('Pop Figurine Black Panther', 15.99, 30, 'Figurine Pop de Black Panther. Cette figurine Pop en PVC mesure 9 cm de hauteur', "../assets/pop black panther.jpg", 'Marvel', 'en stock', 'S', '1102'),
+('Pop Figurine Black and Gold Spiderman', 15.99, 10, 'Figurine Pop de Spiderman avec son costume black and gold. Cette figurine Pop en PVC mesure 9 cm de hauteur.', "../assets/pop black spiderman.jpg", 'Marvel', 'en stock', 'S', '911'),
+('Pop Figurine Bulbizarre', 15.99, 20, 'Figurine Pop de Bulbizarre. Cette figurine Pop en PVC mesure 9 cm de hauteur.', "../assets/pop bulbizarre.jpg", 'Pokémon', 'en stock', 'S', '453'),
+('Pop Figurine Groot', 15.99, 3, 'Figurine Pop de Groot déguisé en Captain America. Cette figurine Pop en PVC mesure 9 cm de hauteur.', "../assets/pop captain groot.jpg", 'Pokémon', 'en stock', 'S', '1392'),
+('Pop Figurine Démogorgon', 15.99, 20, 'Figurine Pop du Démogorgon. Cette figurine Pop en PVC mesure 9 cm de hauteur.', "../assets/pop demogorgon.jpg", 'Stranger Things', 'en stock', 'S', '428'),
+('Pop Figurine Albus Dumbledore', 15.99, 20, 'Figurine Pop Dumbledore en robe rouge. Cette figurine Pop en PVC mesure 9 cm de hauteur.', "../assets/pop dumbledore.jpg", 'Harry Potter', 'en stock', 'S', '04'),
+('Pop Figurine Hedwige', 15.99, 5, 'Figurine Pop Hedwige. Cette figurine Pop en PVC mesure 9 cm de hauteur.', "../assets/pop hedwige.jpg", 'Harry Potter', 'en stock', 'S', '76'),
+('Pop Figurine La Rosalia', 15.99, 5, 'Figurine Pop La Rosalia de son titre La noche de anoche. Cette figurine Pop en PVC mesure 9 cm de hauteur.', "../assets/pop la rosalia.jpg", 'Musique', 'en stock', 'S', '381'),
+('Pop Figurine Pikachu', 15.99, 2, 'Figurine Pop de Pikachu, pokémon le plus célèbre. Cette figurine Pop en PVC mesure 9 cm de hauteur.', "../assets/pop pikachu.jpg", 'Pokémon', 'en stock', 'S', '353'),
+('Pop Figurine Dracaufeu', 39.99, 1, 'Figurine Pop de Dracaufeu. Cette Jumbo Sized Pop en PVC mesure 25 cm de hauteur.', "../assets/pop dracaufeu.jpg", 'Pokémon', 'en stock', 'L', '851'),
+('Pop Figurine Le flagelleur mental', 24.99, 5, 'Figurine Pop du Flagelleur mental. Cette Super Sized Pop en PVC mesure 15 cm de hauteur.', "../assets/pop le flagelleur mental.jpg", 'Stranger Things', 'en stock', 'M', '1597');
